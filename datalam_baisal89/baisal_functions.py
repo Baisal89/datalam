fbddb8c1a16d71730f74eb94645db6852363eacf
baisal89 <baisal89@user.noreply.github.com>
2019-10-29 19:41:02 -0400

    eddited a file

--- datalam_baisal89/baisal_functions.py
+++ datalam_baisal89/baisal_functions.py
@@ -1,11 +1,12 @@
+#import train test split form sklearn
 from sklearn.model_selection import train_test_split
 
 
-
+#defining function find_nulls
 def find_nulls(df):
     return df.isnull().sum()
 
-
+#defining function baisal89_test_split and return it
 def baisal89_test_split(df):
     X_train, X_val, y_train, y_val = train_test_split(
     X_train, y_train, train_size=0.80, test_size=0.20,
